d55fb5fb5502be892d788c04fab273dfa566846e
Marc Bowes <marcbowes@gmail.com>
2011-10-20 11:14:37 +0300

    Use `$!` to report the last Exception. The previous method does not work reliably.

--- lib/safely/backtrace.rb
+++ lib/safely/backtrace.rb
@@ -22,16 +22,9 @@ module Safely
         trace_file = File.join( self.trace_directory, "backtrace-#{Time.now.strftime('%Y%m%d%H%M%S')}-#{Process.pid}.log" )
         trace_log = Logger.new( trace_file )
 
-        # Find the last exception
-        e = nil
-        ObjectSpace.each_object {|o|
-          if ::Exception === o
-            e = o
-          end
-        }
-
+        # Log the last exception
         trace_log.info "*** Below you'll find the most recent exception thrown, this will likely (but not certainly) be the exception that made #{DaemonKit.configuration.daemon_name} exit abnormally ***"
-        trace_log.error e
+        trace_log.error $!
 
         trace_log.info "*** Below you'll find all the exception objects in memory, some of them may have been thrown in your application, others may just be in memory because they are standard exceptions ***"
         ObjectSpace.each_object {|o|
